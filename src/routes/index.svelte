3ef1a0876d2a448a6827d3d4127e4662c0a34f0e
Ansar Nisar <ansarnisargill@gmail.com>
2021-08-01 13:09:03 +0500

    set links

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,6 @@
+<script context="module">
+	export const prerender = true;
+</script>
 <script>
 	import { onMount } from 'svelte';
 	onMount(async() => {
@@ -60,27 +63,13 @@
 		<h3>Find me online:</h3>
 		<ul>
 			<li>
-				<a href="https://www.youtube.com/channel/UC29ju8bIPH5as8OGnQzwJyA" target="_blank"
-					>YouTube</a
-				>
+				<a href="https://twitter.com/ansarnisargill2" target="_blank">Twitter</a>
 			</li>
 			<li>
-				<a href="https://www.udemy.com/user/brad-traversy/" target="_blank">Udemy</a>
+				<a href="https://www.linkedin.com/in/ansarnisar/" target="_blank">LinkedIn</a>
 			</li>
 			<li>
-				<a href="https://twitter.com/traversymedia" target="_blank">Twitter</a>
-			</li>
-			<li>
-				<a href="https://www.linkedin.com/in/bradtraversy/" target="_blank">LinkedIn</a>
-			</li>
-			<li>
-				<a href="https://www.instagram.com/traversymedia/" target="_blank">Instagram</a>
-			</li>
-			<li>
-				<a href="https://github.com/bradtraversy" target="_blank">Github</a>
-			</li>
-			<li>
-				<a href="https://codepen.io/bradtraversy" target="_blank">Codepen</a>
+				<a href="https://github.com/ansarnisargill/" target="_blank">Github</a>
 			</li>
 		</ul>
 	</main>
