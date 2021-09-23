0ee01e673f31f9b52e73df9b45fa1733aa2c2767
Ansar Nisar <ansarnisargill@gmail.com>
2021-09-23 21:51:02 +0500

    Create how-to-rename-all-files-in-a-directory.svelte

---
+++ src/routes/notes/how-to-rename-all-files-in-a-directory.svelte
@@ -0,0 +1,20 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<div class="container">
+	<nav>
+		<ul>
+			<li>
+				<a href="/"> /home </a>
+			</li>
+			<li>
+				<a href="/notes"> /notes </a>
+			</li>
+		</ul>
+	</nav>
+
+	<main>
+		<h1>How to rename all the files in a directory<span class="cursor">|</span></h1>
+	</main>
+</div>
