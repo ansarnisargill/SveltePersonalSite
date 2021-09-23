ecff18a562449ff8d23a4ae088456395dac5244f
Ansar Nisar <ansarnisargill@gmail.com>
2021-09-23 21:51:06 +0500

    Create index.svelte

---
+++ src/routes/notes/index.svelte
@@ -0,0 +1,31 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<div class="container">
+	<nav>
+		<ul>
+			<li>
+				<a href="/"> /home </a>
+			</li>
+		</ul>
+	</nav>
+
+	<main>
+		<h1>Notes:$<span class="cursor">|</span></h1>
+		<h3>List of Posts:</h3>
+		<ol>
+			<li>
+				<a href="/notes/how-to-rename-all-files-in-a-directory">
+					How to rename all the files in a directory
+				</a>
+			</li>
+		</ol>
+	</main>
+</div>
+
+<style>
+	a:hover {
+		text-decoration: underline;
+	}
+</style>
