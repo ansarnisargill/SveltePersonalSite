b1d04453f16d4d78c566a0e4bd3508d548e41ea7
Ansar Nisar <ansarnisargill@gmail.com>
2021-09-24 11:16:58 +0500

    Update how-to-rename-all-files-in-a-directory.svelte

--- src/routes/notes/how-to-rename-all-files-in-a-directory.svelte
+++ src/routes/notes/how-to-rename-all-files-in-a-directory.svelte
@@ -1,11 +1,15 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
 <script>
 	import hljs from 'highlight.js';
-	import {onMount} from 'svelte';
-	onMount(()=>{
-		let el=document.querySelector('code');
-		hljs.highlightElement(el);
+	import { onMount } from 'svelte';
+	onMount(() => {
+		hljs.highlightAll();
 	});
 </script>
+
 <div class="container">
 	<nav>
 		<ul>
@@ -22,8 +26,10 @@
 		<h1>How to rename all the files in a directory<span class="cursor">|</span></h1>
 		<h2>With Powershell:</h2>
 		<p>
-			<code class="language-powershell">Get-ChildItem -Filter "lesson*" -Recurse | Rename-Item -NewName &#123; $_.name -replace `lesson','' &#125;</code>
+			<code class="language-powershell">
+				Get-ChildItem -Filter "search-term*" -Recurse | Rename-Item -NewName &#123; $_.name
+				-replace `old-name','new-name' &#125;
+			</code>
 		</p>
-		
 	</main>
 </div>
